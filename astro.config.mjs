7b07fc3328485d847a34fcf62a26381adf7d1454
Apriantoz <ekaaprianta@gmail.com>
2026-09-03 12:33:23 +0800

    build(config): configure site and base path for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://apriantoz.github.io',
+  base:'/pendopo/',
   vite: {
     plugins: [tailwindcss()]
   }
