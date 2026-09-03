3a03ada65f231919189a838dda60320d3506b44f
apriantoz <ekaaprianta@gmail.com>
2026-09-03 21:20:45 +0800

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://apriantoz.github.io',
-  base: '/pendopo/',
+  site: 'https://pendopo.sanggarsenikeboiwa.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
