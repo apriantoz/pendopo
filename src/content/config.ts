efaeccb2a5d1f73d9c2f599879aa88919162b9f6
apriantoz <ekaaprianta@gmail.com>
2026-09-05 16:56:09 +0800

    Update galeri collection to use glob loader

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const galeri = defineCollection({
-  type: 'data',
+  loader: glob({ pattern: '**/*.json', base: './src/content/galeri' }),
   schema: z.object({
     title: z.string(),
     image: z.string(),
